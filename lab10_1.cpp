3b1aebcc5e3ea4d6f346dd371e39e488e9905be0
nithikon4410 <nithikon_jansanitsri@cmu.ac.th>
2021-01-12 17:23:55 +0700

    lab10_1.cpp

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,16 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<string>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main(){
+    string x[]={"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    srand(time(0));
+    cout<<"Press Enter 3 times to reveal your future."<<endl;
+    cin.get();
+    cin.get();
+    cin.get();
+    int i =rand()%9;
+    cout << "You will get " << x[i] << " " << "in this 261102." ;
+}
